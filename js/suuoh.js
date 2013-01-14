8634501c438c4ca6d05c123741a5a2c0febc3e43
suuoh <melvin.chien@gmail.com>
2013-01-13 23:01:48 -0800

    Fixed fancybox-thumbs src typo and adjusted more fancyBox settings.

--- js/suuoh.js
+++ js/suuoh.js
@@ -1,15 +1,17 @@
 $(document).ready(function() {
   $("a[href$='.jpg'],a[href$='.png']").fancybox({
-    padding : 10,
+    padding: 10,
     margin: 50,
+    loop: false,
     helpers : {
+      title : {
+        type: "over"
+      },
       overlay : {
+        showEarly: false,
         css : {
           "background" : "rgba(25, 25, 25, 0.80)"
         }
-      }, 
-      title: {
-        type: "over"
       }
     }
   });
