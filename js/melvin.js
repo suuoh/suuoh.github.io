297cbd65e34610531e5a0ce260e5dd74830aa471
suuoh <melvin.chien@gmail.com>
2013-09-15 16:35:29 -0400

    First grunt!

--- js/melvin.js
+++ js/melvin.js
@@ -1,50 +1,59 @@
-$(document).ready(function() {
-  $("a[href^='#']").click(function(e){
-    e.preventDefault();
-    var id = $(this).attr("href");
-    var posTop = $(id).position().top;
-    if ($(".hidden-phone").is(":visible"))
-      posTop -= 50;
-    $("html, body").animate({
-      scrollTop: posTop
-    }, 1000);
-  });
-  $("a[href^='http']").attr("target", "_blank");
-  $("a[class='tooltip-hover']").tooltip();
-  $("img.lazy").show().lazyload({
-    effect: "fadeIn"
-  });
-  $("a[href$='.gif'],a[href$='.jpg'],a[href$='.png']").fancybox({
-    padding: 10,
-    margin: 50,
-    loop: false,
-    helpers: {
-      title: {
-        type: "over"
-      },
-      overlay: {
-        showEarly: false,
-        css: {
-          "background" : "rgba(25, 25, 25, 0.80)"
+/*!
+ * melvin.io v1.6.0
+ * Copyright 2013 Melvin Chien <hello@melvin.io> (http://melvin.io/)
+ * Licensed under the MIT license
+ */
+
+(function() {
+  $(document).ready(function() {
+    $('a[href^="#"]').click(function(e) {
+      var id, posTop;
+      e.preventDefault();
+      id = $(this).attr("href");
+      posTop = $(id).position().top;
+      if ($(".hidden-phone").is(":visible")) {
+        posTop -= 50;
+      }
+      return $("html, body").animate({
+        scrollTop: posTop
+      }, 1000);
+    });
+    $('a[href^="http"]').attr("target", "_blank");
+    $('a[class="tooltip-hover"]').tooltip();
+    $("img.lazy").show().lazyload({
+      effect: "fadeIn"
+    });
+    $('a[href$=".gif"],a[href$=".jpg"],a[href$=".png"]').fancybox({
+      padding: 10,
+      margin: 50,
+      loopo: false,
+      helpers: {
+        title: {
+          type: "over"
+        },
+        overlay: {
+          showEarly: false,
+          css: background("rgba(25, 25, 25, 0.80)")
         }
       }
-    }
-  });
-  $(".fancybox-media").fancybox({
-    padding: 10,
-    margin: 50,
-    loop: false,
-    helpers: {
-      media: {},
-      title: {
-        type: "float"
-      },
-      overlay: {
-        showEarly: false,
-        css: {
-          "background" : "rgba(25, 25, 25, 0.80)"
+    });
+    return $(".fanxybox-media").fancybox({
+      padding: 10,
+      margin: 50,
+      loop: false,
+      helpers: {
+        media: {},
+        title: {
+          type: "float"
+        },
+        overlay: {
+          showEarly: false,
+          css: {
+            background: "rgba(25, 25, 25, 0.80)"
+          }
         }
       }
-    }
+    });
   });
-});
+
+}).call(this);
