5653c385c7a9f56f34517edd14499e66d17c500c
suuoh <me@melvin.io>
2017-08-07 12:16:10 -0400

    Archiving current version

---
+++ v1.7/js/melvin.coffee
@@ -0,0 +1,86 @@
+$(document).ready ->
+  # Initialize Foundation
+  $(this).foundation()
+
+  # Fixes for Foundation's mobile navbar
+  $("#js-navbar .name a, #js-navbar .top-bar-section a").click ->
+    topbar = $(".topbar, [data-topbar]")
+
+    # Make mobile navbar stay fixed to top of window
+    # Code from foundation.topbar.js:60
+    topbar.removeClass "fixed"
+    topbar.parent().addClass "fixed"
+    $("body").addClass "f-topbar-fixed"
+
+    # Close expanded navbar after an item is clicked
+    # Code from foundation.topbar.js:215
+    topbar.css "height", ""
+    topbar.removeClass "expanded"
+    topbar.find("li").removeClass "hover"
+
+  # Remove clear navbar if page is scrolled down
+  $(window).scroll ->
+      if $(this).scrollTop() > 45
+        $(".top-bar").removeClass "top-bar-clear"
+      else
+        $(".top-bar").addClass "top-bar-clear"
+
+  # Bind jQuery Address deep link click event handler
+  $("#js-navbar, #js-container").on "click", ".deep-link", (e) ->
+    e.preventDefault()
+    href = $(this).attr "href"
+    # Remove hashes
+    href = href.replace /^#/, ""
+    # Replace hyphens with forward slash
+    href = href.replace "-", "/"
+    # Set new address
+    $.address.value href
+
+  # Bind jQuery Address address change event handler
+  $.address.change (e) ->
+    container = $("#js-container")
+    content = $("#js-content")
+    footer = $("#js-footer")
+    divider = $("#js-hexagon")
+    title = "melvin"
+    href = e.value
+    # Remove leading forward slash
+    href = href.replace /^\//, ""
+    # Remove trailing forward slash
+    href = href.replace /\/$/, ""
+    url = href.split "/"
+
+    # Set new page title
+    for word in url
+      if word isnt ""
+        switch word
+          when "anteaternetwork"
+            word = "Anteater Network"
+          when "jmmp"
+            word = "JMMP"
+          when "alienescape"
+            word = "Alien Escape"
+          else
+            word = word.charAt(0).toUpperCase() + word.slice 1
+        title = word + " · " + title
+    $.address.title title
+
+    # Load appropriate content
+    if href is ""
+      footer.fadeOut "500"
+      content.fadeOut "500", ->
+        container.hide()
+      container.removeClass "active"
+    else
+      container.show()
+      href = href.replace "/", "-"
+      footer.fadeOut "250"
+      content.fadeOut "250", ->
+        content.load "/" + href + ".html", (response, status, xhr) ->
+          if status is "error"
+            # Redirect to page without hashtag (which then redirects to 404)
+            window.location.replace "/" + href
+          else
+            footer.fadeIn "500"
+            content.fadeIn "2000"
+            container.addClass "active"
---
+++ v1.7/js/melvin.js
@@ -0,0 +1,85 @@
+(function() {
+  $(document).ready(function() {
+    $(this).foundation();
+    $("#js-navbar .name a, #js-navbar .top-bar-section a").click(function() {
+      var topbar;
+      topbar = $(".topbar, [data-topbar]");
+      topbar.removeClass("fixed");
+      topbar.parent().addClass("fixed");
+      $("body").addClass("f-topbar-fixed");
+      topbar.css("height", "");
+      topbar.removeClass("expanded");
+      return topbar.find("li").removeClass("hover");
+    });
+    $(window).scroll(function() {
+      if ($(this).scrollTop() > 45) {
+        return $(".top-bar").removeClass("top-bar-clear");
+      } else {
+        return $(".top-bar").addClass("top-bar-clear");
+      }
+    });
+    $("#js-navbar, #js-container").on("click", ".deep-link", function(e) {
+      var href;
+      e.preventDefault();
+      href = $(this).attr("href");
+      href = href.replace(/^#/, "");
+      href = href.replace("-", "/");
+      return $.address.value(href);
+    });
+    return $.address.change(function(e) {
+      var container, content, divider, footer, href, i, len, title, url, word;
+      container = $("#js-container");
+      content = $("#js-content");
+      footer = $("#js-footer");
+      divider = $("#js-hexagon");
+      title = "melvin";
+      href = e.value;
+      href = href.replace(/^\//, "");
+      href = href.replace(/\/$/, "");
+      url = href.split("/");
+      for (i = 0, len = url.length; i < len; i++) {
+        word = url[i];
+        if (word !== "") {
+          switch (word) {
+            case "anteaternetwork":
+              word = "Anteater Network";
+              break;
+            case "jmmp":
+              word = "JMMP";
+              break;
+            case "alienescape":
+              word = "Alien Escape";
+              break;
+            default:
+              word = word.charAt(0).toUpperCase() + word.slice(1);
+          }
+          title = word + " · " + title;
+        }
+      }
+      $.address.title(title);
+      if (href === "") {
+        footer.fadeOut("500");
+        content.fadeOut("500", function() {
+          return container.hide();
+        });
+        return container.removeClass("active");
+      } else {
+        container.show();
+        href = href.replace("/", "-");
+        footer.fadeOut("250");
+        return content.fadeOut("250", function() {
+          return content.load("/" + href + ".html", function(response, status, xhr) {
+            if (status === "error") {
+              return window.location.replace("/" + href);
+            } else {
+              footer.fadeIn("500");
+              content.fadeIn("2000");
+              return container.addClass("active");
+            }
+          });
+        });
+      }
+    });
+  });
+
+}).call(this);
