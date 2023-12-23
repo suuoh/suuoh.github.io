7deb9e21da68520a0a823aa0eb249240074ed3f7
Melvin Chien <me@melvin.io>
2023-12-23 18:11:17 -0500

    Fix contact form

--- js/melvin.js
+++ js/melvin.js
@@ -1,5 +1,5 @@
 /*!
- * melvin.io v1.8
+ * melvin.io v2.0
  * Licensed under the MIT license
  */
 
@@ -27,35 +27,66 @@ $(document).ready(function() {
     });
   });
 
-  $("#contactForm").submit(function(event) {
-    event.preventDefault();
-    $.ajax({
-      url: "https://formspree.io/me@melvin.io",
-      method: "POST",
-      dataType: "JSON",
-      data: $("#contactForm").serialize(),
-      success: function() {
-        $("#contactAlert")
-          .html("<strong>Success!</strong> Your message was sent. I will get back to you soon.")
-          .show()
-          .addClass("alert-success show")
-          .removeClass("alert-danger")
-          .delay(3000)
-          .slideUp(500, function() {
-            $(this).removeClass("show");
-          });
-      },
-      error: function() {
-        $("#contactAlert")
-          .html("<strong>Oops!</strong> Something went wrong. Please try sending your message again or emailing me directly instead.")
-          .show()
-          .addClass("alert-danger show")
-          .removeClass("alert-success")
-          .delay(3000)
-          .slideUp(500, function() {
-            $(this).removeClass("show");
-          });
-      }
-    });
-  });
+  var contactForm = document.getElementById("contactForm");
+
+  async function handleSubmit(event) {
+      event.preventDefault();
+      var contactAlert = document.getElementById("contactAlert");
+      var data = new FormData(event.target);
+      fetch(event.target.action, {
+        method: contactForm.method,
+        body: data,
+        headers: {
+            'Accept': 'application/json'
+        }
+      }).then(response => {
+        if (response.ok) {
+          contactAlert.style.display = '';
+          contactAlert.classList.add("alert-success");
+          contactAlert.classList.add("show");
+          contactAlert.innerHTML = "<strong>Thanks for your message!</strong> I will get back to you as soon as I can.";
+          setTimeout(function() {
+            contactAlert.classList.remove("show");
+            contactAlert.classList.remove("alert-success");
+            setTimeout(function() {
+              contactForm.reset();
+              contactAlert.style.display = 'none';
+            }, 300)
+          }, 3000)
+        } else {
+          response.json().then(data => {
+            contactAlert.style.display = '';
+            contactAlert.classList.add("alert-danger");
+            contactAlert.classList.add("show");
+            if (Object.hasOwn(data, 'errors')) {
+              contactAlert.innerHTML = "<strong>" + data["errors"].map(error => error["message"]).join(", ") + "!</strong> Please try again or email me directly instead."
+            } else {
+              contactAlert.innerHTML = "<strong>Something went wrong!</strong> Please email me directly instead."
+            }
+            setTimeout(function() {
+            contactAlert.classList.remove("show");
+            contactAlert.classList.remove("alert-danger");
+            setTimeout(function() {
+              contactForm.reset();
+              contactAlert.style.display = 'none';
+            }, 300)
+          }, 3000)
+          })
+        }
+      }).catch(error => {
+        contactAlert.style.display = '';
+        contactAlert.classList.add("alert-danger");
+        contactAlert.classList.add("show");
+        contactAlert.innerHTML = "<strong>Something went wrong!</strong> Please email me directly instead."
+        setTimeout(function() {
+          contactAlert.classList.remove("show");
+          contactAlert.classList.remove("alert-danger");
+          setTimeout(function() {
+            contactForm.reset();
+            contactAlert.style.display = 'none';
+          }, 300)
+        }, 3000)
+      });
+    }
+  contactForm.addEventListener("submit", handleSubmit)
 });
